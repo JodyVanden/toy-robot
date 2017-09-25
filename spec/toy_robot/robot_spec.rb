4a55987ed3aa7b4a9bc7067c64eb9dd48a601a3c
Jody Vanden <jody.vanden@gmail.com>
2017-09-25 15:11:28 +1000

    add tests moving_up and change position to position_x and position_y

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -5,21 +5,31 @@ describe ToyRobot::Robot do
 
   it "moves forward 4 space" do
     4.times {subject.move_forward}
-    expect(subject.position).to eq(4)
+    expect(subject.position_x).to eq(4)
   end
 
   it "moves forward 2 space" do
     2.times {subject.move_forward}
-    expect(subject.position).to eq(2)
+    expect(subject.position_x).to eq(2)
   end
 
   it "moves backward 2 space" do
     2.times {subject.move_backward}
-    expect(subject.position).to eq(-2)
+    expect(subject.position_x).to eq(-2)
   end
 
   it "moves backward 4 space" do
     4.times {subject.move_backward}
-    expect(subject.position).to eq(-4)
+    expect(subject.position_x).to eq(-4)
+  end
+
+  it "moves up 2 space" do
+    2.times {subject.move_up}
+    expect(subject.position_y).to eq(2)
+  end
+
+  it "moves up 4 space" do
+    4.times {subject.move_up}
+    expect(subject.position_y).to eq(4)
   end
 end
