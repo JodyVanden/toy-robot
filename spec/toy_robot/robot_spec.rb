6b8edaba4cc88504f13f9098fa64871dc576bfdb
Jody Vanden <jody.vanden@gmail.com>
2017-09-25 15:08:55 +1000

    modify robot_spec to be clearer on forward and backward

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -3,23 +3,23 @@ require "spec_helper"
 describe ToyRobot::Robot do
   subject {ToyRobot::Robot.new(0)}
 
-  it "moves 4 space" do
+  it "moves forward 4 space" do
     4.times {subject.move_forward}
     expect(subject.position).to eq(4)
   end
 
-  it "moves 2 space" do
+  it "moves forward 2 space" do
     2.times {subject.move_forward}
     expect(subject.position).to eq(2)
   end
 
-  it "moves 2 space" do
+  it "moves backward 2 space" do
     2.times {subject.move_backward}
     expect(subject.position).to eq(-2)
   end
 
-  it "moves 2 space" do
-    2.times {subject.move_backward}
-    expect(subject.position).to eq(-2)
+  it "moves backward 4 space" do
+    4.times {subject.move_backward}
+    expect(subject.position).to eq(-4)
   end
 end
