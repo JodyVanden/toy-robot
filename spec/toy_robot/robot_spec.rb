aa813fd2283821de6f1893af98c9923fda15084f
Jody Vanden <jody.vanden@gmail.com>
2017-09-25 14:42:35 +1000

    add first test for robot move

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -1,2 +1,10 @@
 require "spec_helper"
 
+describe ToyRobot::Robot do
+  subject {ToyRobot::Robot.new(0)}
+
+  it "moves 4 space" do
+    4.times {suject.move}
+    expect {suject.position}.to eq(4)
+  end
+end
