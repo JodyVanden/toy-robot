99e1fa3a514d481dd859ad8e91da1844fe77405d
Jody Vanden <jody.vanden@gmail.com>
2017-09-25 14:57:25 +1000

    implement move and tests

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -4,7 +4,7 @@ describe ToyRobot::Robot do
   subject {ToyRobot::Robot.new(0)}
 
   it "moves 4 space" do
-    4.times {suject.move}
-    expect {suject.position}.to eq(4)
+    4.times {subject.move}
+    expect(subject.position).to eq(4)
   end
 end
