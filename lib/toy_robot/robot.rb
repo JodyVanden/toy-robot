5fe5d95a9243802ed289ec2efa7b1a654b16e506
Jody Vanden <jody.vanden@gmail.com>
2017-09-25 15:14:38 +1000

    change in lib/toy_robot/robot position in position_x and position_y and add move_up

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,17 +1,22 @@
 module ToyRobot
   class Robot
-    attr_reader :position
+    attr_reader :position_x, :position_y
 
-    def initialize(position = 0)
-      @position = position
+    def initialize(position_x = 0, position_y = 0)
+      @position_x = position_x
+      @position_y = position_y
     end
 
     def move_forward
-      @position += 1
+      @position_x += 1
     end
 
     def move_backward
-      @position -= 1
+      @position_x -= 1
+    end
+
+    def move_up
+      @position_y += 1
     end
 
   end
