5f67100871602a8e390e59ae56863c948c95726d
Jody Vanden <jody.vanden@gmail.com>
2017-09-25 16:39:21 +1000

    table add initialize and tests to check the position of the robot. Robot should not be outside of the table

--- lib/toy_robot/table.rb
+++ lib/toy_robot/table.rb
@@ -1,5 +1,10 @@
 module ToyRobot
   class Table
 
+    def initialize(length = 5, width = 5)
+      @length = length
+      @width = width
+    end
+
   end
 end
--- spec/toy_robot/table_spec.rb
+++ spec/toy_robot/table_spec.rb
@@ -1,3 +1,10 @@
 require "spec_helper"
 
+describe ToyRobot::Table do
+  subject { ToyRobot::Table.new(5, 5) }
 
+  context "valid_position?" do
+  it { should be_valid_position(3, 0) }
+  end
+
+end
